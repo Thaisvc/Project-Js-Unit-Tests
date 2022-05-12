052998b5b64fbdcba6ac436c14752bede67f966c
thais <thaisviana081@outlook.com>
2022-05-12 12:54:18 -0300

    requisito 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -32,9 +32,20 @@
   arrayGenerator('values', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ 3, 2, 1, 0 ]
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
-
-const calculator = (number1, number2) => {};
-
-const arrayGenerator = (type, object) => {};
+// Math.floor = https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+const calculator = (number1, number2) => ({
+  sum: number1 + number2,
+  mult: number1 * number2,
+  div: Math.floor(number1 / number2),
+  sub: number1 - number2,
+  
+});
+/* console.log(calculator(1, 2)); */
+// object keys,value e entries = https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+const arrayGenerator = (tipoConvert, object) => {
+  if (tipoConvert === 'keys') return Object.keys(object);
+  if (tipoConvert === 'values') return Object.values(object);
+  if (tipoConvert === 'entries') return Object.entries(object);
+ };
 
 module.exports = { calculator, arrayGenerator };
